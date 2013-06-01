2da5867a23b29067f92aae0c9d333c1cda829ca9
sliu99 <sliu99@yahoo.com>
2013-06-01 09:21:50 -0600

    win: comment File1

--- src/com/home/github/File1.java
+++ src/com/home/github/File1.java
@@ -2,11 +2,8 @@ package com.home.github;
 
 public class File1 {
 
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		File1 file1 = new File1();
+		//File1 file1 = new File1();
 
 	}
 
